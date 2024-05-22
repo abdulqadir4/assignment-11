e3fdfb04c0c8f8c6b45c19d632ffe3244dc49bce
abdulqadir4 <154599297+abdulqadir4@users.noreply.github.com>
2024-05-22 05:23:46 -0700

    Add files via upload

---
+++ assignment 11/styles.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    // Here you can add further form submission logic, like sending data to a server
+});
